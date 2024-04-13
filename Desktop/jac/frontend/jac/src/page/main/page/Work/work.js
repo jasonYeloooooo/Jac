210ebc81c35e0fb2386175c429d59ca0cdaba0f4
jasonYeloooooo <U7543349@anu.edu.au>
2024-04-14 00:24:38 +1000

    add work page

--- Desktop/jac/frontend/jac/src/page/main/page/Work/work.js
+++ Desktop/jac/frontend/jac/src/page/main/page/Work/work.js
@@ -1,27 +1,26 @@
 import dayjs from 'dayjs'
-import React, { useState } from 'react'
-import { CalendarPicker, Card ,Picker,Button} from 'antd-mobile'
+import React, { useState ,useCallback} from 'react'
+import { Button,List,CheckList,DatePickerView,Toast} from 'antd-mobile'
 import axios from 'axios'
 
 
 
 export default function Work(){
-  const day = dayjs()
-  const [val, setVal] = useState(day.format("YYYY-MM-DD"))
+  const [val, setVal] = useState()
   const [firstShiftStart,setFirstStart] = useState()
   const [firstShiftEnd,setFirstEnd] = useState()
   const [secondShiftStart,setSecondStart] = useState()
   const [secondShiftEnd,setSecondEnd] = useState()
+  const [start,setStart] = useState(true)
+  const [end,setEnd] = useState(false)
+  const [shiftStart,setShiftStart] = useState()
+  const [shiftEnd,setShiftEnd] = useState()
+  const [first,setFirst] = useState(true)
+  const [second,setSecond] = useState(false)
+  const now = new Date()
 
-  const [visible, setVisible] = useState(false)
-  const selected =(Date)=>{
-      const d = dayjs(Date)
-      setVal(d.format('YYYY-MM-DD'))
-      console.log(d.format('YYYY-MM-DD'))
-  }
 
-  const singleDate = new Date()
-
+  
   const postData = {
         name: "jason",
         date: val,
@@ -44,124 +43,128 @@ export default function Work(){
       });
 }
 
-  //const chooseFirstShiftStart =(value)=>{console.log(firstStart)}
-  
-  const dateClick = ()=>{
-    setVisible(true)
-  }
 
+  const labelRenderer = useCallback((type: string, data: number) => {
+    switch (type) {
+      case 'year':
+        return data + '年'
+      case 'month':
+        return data + '月'
+      case 'day':
+        return data + '日'
+      case 'hour':
+        return data + '时'
+      case 'minute':
+        return data + '分'
+      case 'second':
+        return data + '秒'
+      default:
+        return data
+    }
+  }, [])
+
+
+  
   return (
+    
     <div style={{display:"flex",flexDirection:"column",justifyContent:"space-between"}}>
     
+    <List header='工作时间'>
+      <CheckList  defaultValue={['First']} onChange={val => {
+              if(val[0]==="First"){
+                setFirst(true)
+                setSecond(false)
+
+                setShiftStart(firstShiftStart)
+                setShiftEnd(firstShiftEnd)
+                console.log("First")
+              }
+              if(val[0]==="Second"){
+                setFirst(false)
+                setSecond(true)
+                setShiftStart(secondShiftStart)
+                setShiftEnd(secondShiftEnd)
+                console.log("second")
+              }
+              if(val[0]==="Break") {
+                
+                console.log("not good")
+              }
+            }}>
+            <CheckList.Item value='First'>First Shift</CheckList.Item>
+            <CheckList.Item value='Second'>Second Shift</CheckList.Item>
+            <CheckList.Item  value='Break'>break</CheckList.Item>
+
+      </CheckList>
+    </List>
+
+   
+      <List header='休息时间'>
+        <CheckList  defaultValue={['start']} activeIcon={<div /> }onChange={val => {
+              if(val[0]==="start"){
+                setStart(true)
+                setEnd(false)
+                console.log("start")
+              }
+              if(val[0]==="end") {
+                setStart(false)
+                setEnd(true)
+                console.log("end")
+              }
+            }}>
+            <CheckList.Item value='start'><div>开始 <span style={{float: "right"}}>{shiftStart}</span></div></CheckList.Item>
+            <CheckList.Item value='end'><div>结束 <span style={{float: "right"}}>{shiftEnd}</span></div></CheckList.Item>
+       </CheckList>
      
-    <Card title="选择日期" onClick={dateClick}>{val}</Card>
-        <CalendarPicker
-          visible={visible}
-          selectionMode='single'
-          defaultValue={singleDate}
-          onClose={() => setVisible(false)}
-          onMaskClick={() => setVisible(false)}
-          onConfirm={selected}
+      </List> 
+
+
+
+   
+      <DatePickerView
+          visible={true}
+          defaultValue={now}
+          precision='minute'
+          onConfirm={val => {
+            Toast.show(val.toString())
+          }}
+          renderLabel={labelRenderer}
+          onChange={val=>{ 
+            const date = new Date(val)
+            const d = dayjs(date)
+            setVal(d.format('YYYY-MM-DD'))
+            console.log(d.format('YYYY-MM-DD'))
+            console.log(d.format('HH:mm:ss'))
+
+            if(first){
+              if(start){setFirstStart(d.format('HH:mm:ss'));setShiftStart(d.format('MM月DD日 HH:mm:ss'))}
+              else if(end){ setFirstEnd(d.format('HH:mm:ss'));setShiftEnd(d.format('MM月DD日 HH:mm:ss'))}
+            }
+            else if(second){
+              if(start){setSecondStart(d.format('HH:mm:ss'));setShiftStart(d.format('MM月DD日 HH:mm:ss'))}
+              else if(end){ setSecondEnd(d.format('HH:mm:ss'));setShiftEnd(d.format('MM月DD日 HH:mm:ss')) }
+            }
+           
+          
+          
+          }}
         />
+       
 
 
-    <div style={{display:"flex",justifyContent:"space-between",alignItems:"baseline",flexWrap:"wrap",marginTop:"20px"}}>    
-      <Picker
-       columns={basicColumns}
+       <Button size='middle' color='primary' onClick={submitWork}>
+            Middle
+          </Button>
 
-        onConfirm={v => {
-          let res = v[0]+':'+v[1]+':00'
-          setFirstStart(res)
-        }}
-    >
-      {(_, actions) => <Button onClick={actions.open}>choose the the first shift start</Button>}
-    </Picker>
-    {firstShiftStart}
-        {" to "}
-    {firstShiftEnd}
-    <Picker
-       columns={basicColumns}
-        onConfirm={v => {
-          let res = v[0]+':'+v[1]+':00'
-          setFirstEnd(res)
-        }}
-    >
-      {(_, actions) => <Button onClick={actions.open}>choose the the first shift end</Button>}
-    </Picker>
-   
 
-    </div>
 
-    <div style={{display:"flex",justifyContent:"space-between",alignItems:"baseline",flexWrap:"wrap",marginTop:"10px"}}> 
-         <Picker
-       columns={basicColumns}
 
-        onConfirm={v => {
-          let res = v[0]+':'+v[1]+':00'
-          setSecondStart(res)
-        }}
-    >
-      {(_, actions) => <Button onClick={actions.open}>choose the the second shift start</Button>}
-    </Picker>
-   
-    {secondShiftStart}
-        {" to "}
-    {secondShiftEnd}
 
-    <Picker
-       columns={basicColumns}
 
-        onConfirm={v => {
-          let res = v[0]+':'+v[1]+':00'
-          setSecondEnd(res)
-        }}
-    >
-      {(_, actions) => <Button onClick={actions.open}>choose the the second shift end</Button>}
-    </Picker>
-  
-  </div>
 
-  <Button block color='primary' size='large' style={{position: "relative",marginTop:"100px"}} onClick={submitWork}>
-          submit
-   </Button>
+
+
 
     </div>
   )
 }
-
-export const basicColumns = [
-  [
-    { label: '10', value: '10' },
-    { label: '11', value: '11' },
-    { label: '12', value: '12' },
-    { label: '13', value: '13' },
-    { label: '14', value: '14' },
-    { label: '15', value: '15' },
-    { label: '16', value: '16' },
-    { label: '17', value: '17' },
-    { label: '18', value: '18' },
-    { label: '19', value: '19' },
-    { label: '20', value: '20' },
-    { label: '21', value: '21' },
-    { label: '22', value: '22' },
-    { label: '23', value: '23' },
-  ],
-  [
-    { label: '00', value: '00' },
-    { label: '10', value: '10' },
-    { label: '15', value: '15' },
-    { label: '20', value: '20' },
-    { label: '25', value: '25' },
-    { label: '30', value: '30' },
-    { label: '35', value: '35' },
-    { label: '40', value: '40' },
-    { label: '45', value: '45' },
-    { label: '50', value: '50' },
-    { label: '55', value: '55' },
-
-
-
-
-
-  ],
-]
